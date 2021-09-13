af1a408710e090a72290e84fa3d2da23a74682c0
Alfir Tyakin <alfir777@rambler.ru>
2021-09-13 15:42:01 +0500

    add pagination

--- config/blog/views.py
+++ config/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
 
 from config.settings import DOMAIN_NAME
 from .models import Post, Category, Tag
@@ -10,7 +11,7 @@ class Blog(ListView):
     model = Post
     template_name = 'blog/blog.html'
     context_object_name = 'posts'
-    paginate_by = 10
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
