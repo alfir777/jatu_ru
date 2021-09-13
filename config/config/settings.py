59235df47c2aa9009786ab6fa6427163a249ce02
Alfir Tyakin <alfir777@rambler.ru>
2021-09-13 18:45:35 +0500

    fix ckeditor and navbar

--- config/config/settings.py
+++ config/config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'ckeditor',
+    'ckeditor_uploader',
     'blog.apps.BlogConfig',
 ]
 
@@ -148,7 +149,7 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 
 CKEDITOR_CONFIGS = {
     'default': {
-        'skin': 'moono-liza',
+        'skin': 'moono-lisa',
         # 'skin': 'office2013',
         'toolbar_Basic': [
             ['Source', '-', 'Bold', 'Italic']
