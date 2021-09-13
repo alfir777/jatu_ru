423ac155d62e9b49928f976efa4bece06234b704
Alfir Tyakin <alfir777@rambler.ru>
2021-09-13 21:32:50 +0500

    add tag cloud

--- config/blog/templatetags/sidebar.py
+++ config/blog/templatetags/sidebar.py
@@ -1,6 +1,6 @@
 from django import template
 
-from blog.models import Post
+from blog.models import Post, Tag
 
 register = template.Library()
 
@@ -9,3 +9,9 @@ register = template.Library()
 def get_popular(cnt=3):
     posts = Post.objects.order_by('-views')[:cnt]
     return {'posts': posts}
+
+
+@register.inclusion_tag('blog/tags_tpl.html')
+def get_list_tags():
+    tags = Tag.objects.all()
+    return {'tags': tags}
