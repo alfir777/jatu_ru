b59f509dee37c5ae89d155675305f27f6b5be4b3
Alfir Tyakin <alfir777@rambler.ru>
2021-09-13 21:56:55 +0500

    add search

--- config/blog/urls.py
+++ config/blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('blog/category/<str:slug>', PostByCategory.as_view(), name='category'),
     path('blog/tag/<str:slug>', PostByTag.as_view(), name='tag'),
     path('post/<str:slug>', GetPost.as_view(), name='post'),
+    path('search/', Search.as_view(), name='search'),
     path('test/', test, name='test'),
 ]
