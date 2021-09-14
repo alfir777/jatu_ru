b0addf62d33b12eae20f2e3946bfbbb14171a4db
Alfir Tyakin <alfir777@rambler.ru>
2021-09-14 14:15:43 +0500

    add register/login/logout

--- config/blog/urls.py
+++ config/blog/urls.py
@@ -4,12 +4,15 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('portfolio/', portfolio, name='portfolio'),
     path('blog/', Blog.as_view(), name='blog'),
     path('contact/', contact, name='contact'),
+    path('login/', user_login, name='user_login'),
+    path('logout/', user_logout, name='user_logout'),
+    path('portfolio/', portfolio, name='portfolio'),
+    path('register/', register, name='register'),
+    path('search/', Search.as_view(), name='search'),
+    path('test/', test, name='test'),
     path('blog/category/<str:slug>', PostByCategory.as_view(), name='category'),
     path('blog/tag/<str:slug>', PostByTag.as_view(), name='tag'),
     path('post/<str:slug>', GetPost.as_view(), name='post'),
-    path('search/', Search.as_view(), name='search'),
-    path('test/', test, name='test'),
 ]
--- config/blog/views.py
+++ config/blog/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import logout, login
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from django.db.models import F
 
 from config.settings import DOMAIN_NAME
+from .forms import UserRegisterForm, UserLoginForm
 from .models import Post, Category, Tag
 
 
@@ -11,7 +14,7 @@ class Blog(ListView):
     model = Post
     template_name = 'blog/blog.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 10
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -22,7 +25,7 @@ class Blog(ListView):
 class PostByCategory(ListView):
     template_name = 'blog/category.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 10
     allow_empty = False
 
     def get_queryset(self):
@@ -37,7 +40,7 @@ class PostByCategory(ListView):
 class PostByTag(ListView):
     template_name = 'blog/tags.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 10
     allow_empty = False
 
     def get_queryset(self):
@@ -65,7 +68,7 @@ class GetPost(DetailView):
 class Search(ListView):
     template_name = 'blog/search.html'
     context_object_name = 'posts'
-    paginate_by = 1
+    paginate_by = 10
 
     def get_queryset(self):
         return Post.objects.filter(title__icontains=self.request.GET.get('s'))
@@ -74,6 +77,38 @@ class Search(ListView):
         context = super().get_context_data(**kwargs)
         context['s'] = f"s={self.request.GET.get('s')}&"
         return context
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect('home')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('user_login')
 
 
 def index(request):
