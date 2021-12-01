24711080612825fc532f9cbbbe5c34523277436a
Alfir Tyakin <alfir777@rambler.ru>
2021-12-01 16:11:20 +0500

    fix model post

--- config/blog/models.py
+++ config/blog/models.py
@@ -61,14 +61,14 @@ class Tag(BaseModel):
 
 
 class Post(BaseModel):
-    title = models.CharField(max_length=100, verbose_name='Заголовок')
-    description = models.TextField(max_length=100, verbose_name='Описание', default='')
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    description = models.TextField(max_length=255, verbose_name='Описание', default='')
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='posts')
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-    photo = models.ImageField(upload_to='photos/%Y/%M/%d/', blank=True, verbose_name='Фото')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
     views = models.IntegerField(default=0, verbose_name='Количество просмотров')
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
